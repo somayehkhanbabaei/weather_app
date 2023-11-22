683d0a09add40b73daf47193a7873bec92511761
Somayeh <somayeh.khanbabaei@gmail.com>
2023-11-22 13:31:10 +0330

    add github link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ function App() {
   return (
     <div className='App'>
       <Weather city='Utrecht' />
+      <p>
+        Create by Somayeh Khanbabaei{' '}
+        <a href='https://github.com/somayehkhanbabaei/weather_app'>
+          Open Source
+        </a>
+      </p>
     </div>
   );
 }
